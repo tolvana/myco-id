bb563c5b690fe1d092995e9d02ec6b882e3b7d78
Arttu Tolvanen <arttu@tolvanen.xyz>
2024-06-04 20:21:36 +0300

    Add wiki and gbif links

--- frontend/src/components/Results.tsx
+++ frontend/src/components/Results.tsx
@@ -1,43 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Results.module.css';
+import WikipediaIcon from '../icons/wikipedia';
+import GbifIcon from '../icons/gbif';
 
 // Props type definition
 type ResultsProps = {
   results: [string, any][];
 };
 
-// Declare the functional component with proper props handling
 const Results: React.FC<ResultsProps> = ({ results }) => {
-
-    // dict state for species data (keyed by scientific name)
-    const [speciesData, setSpeciesData] = useState<Record<string, any>>({});
-
-    // In your Results component
-    return (
+  return (
     <div className={styles.mainContainer}>
-    {results.map(([scientificName, {probability, info}]) => (
-      <div key={scientificName} className={styles.resultEntry}>
-        <div className={styles.resultLabel}>
-          {info.common_names.eng ? (
-            <>
-              <span className={styles.vernacularName}>{info.common_names["eng"][0]}</span>
-              <span> (</span>
-              <span className={styles.scientificName}>{scientificName}</span>
-              <span>)</span>
-            </>
-          ) : (
-            <span className={styles.scientificName}>{scientificName}</span>
-          )}
-        </div>
-        <div className={styles.progressBar}>
-          <div className={styles.progressBarFill} style={{ width: `${probability * 100}%` }}>
-            {`${(probability * 100).toFixed(1)}%`}
+      {results.map(([scientificName, { probability, info }]) => {
+
+        const normalizedName = scientificName.replace(" ", "_");
+        const gbifId = info.gbif_id;
+        const wikiUrl = `https://en.wikipedia.org/wiki/${encodeURIComponent(normalizedName)}`;
+        const gbifUrl = `https://www.gbif.org/species/${encodeURIComponent(gbifId)}`;
+
+        return (
+          <div key={scientificName} className={styles.resultEntry}>
+            <div className={styles.resultLabel}>
+              <div className={styles.textContainer}>
+                {info.common_names.eng ? (
+                  <>
+                    <span className={styles.vernacularName}>{info.common_names["eng"][0]}</span>
+                    <span> (</span>
+                    <span className={styles.scientificName}>{scientificName}</span>
+                    <span>)</span>
+                  </>
+                ) : (
+                  <span className={styles.scientificName}>{scientificName}</span>
+                )}
+              </div>
+
+              {/* Aligning the icons to the right */}
+              <div className={styles.icons}>
+                <a href={wikiUrl} target="_blank" rel="noopener noreferrer" className={styles.icon}>
+                  <WikipediaIcon />
+                </a>
+                <a href={gbifUrl} target="_blank" rel="noopener noreferrer" className={styles.icon}>
+                  <GbifIcon />
+                </a>
+              </div>
+            </div>
+            <div className={styles.progressBar}>
+              <div className={styles.progressBarFill} style={{ width: `${probability * 100}%` }}>
+                {`${(probability * 100).toFixed(1)}%`}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    ))}
+        );
+      })}
     </div>
-    );
+  );
 };
 
 export default Results;
